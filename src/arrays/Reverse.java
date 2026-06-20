376279991471f4dcd5a5d6aa2614f692fec69cff
Shifa Mugasheena.A <shifamugasheena26@gmail.com>
2026-06-20 12:56:20 +0530

    Feat:add reverse program

---
+++ src/arrays/Reverse.java
@@ -0,0 +1,30 @@
+package arrays;
+
+public class Reverse {
+    public static void main(String[] args) {
+
+        int[] arr = {800, 9, 87, 29, 111};
+        System.out.println("Before Reversing");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+        reverse(arr , 0 , arr.length-1);
+
+        System.out.println("After Reversing");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void reverse(int[] arr, int start, int end) {
+        while (start <= end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+
+        }
+    }
+}
+
